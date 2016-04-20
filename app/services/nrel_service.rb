11efcae6fbedda02aabd78c91c44b548363f0f99
July Tran <julyytran@gmail.com>
2016-04-20 12:44:12 -0600

    renderStates is working

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -54,6 +54,6 @@ class NRELService
       "OK" => "Oklahoma", "OR" => "Oregon", "PA" => "Pennsylvania", "RI" => "Rhode Island",
       "SC" => "South Carolina", "SD" => "South Dakota", "TN" => "Tennessee", "TX" => "Texas",
       "UT" => "Utah", "VT" => "Vermont", "VA" => "Virginia", "WA" => "Washington",
-      "WV" => "West Virginia", "WI" => "Wisconsin", "WY" => "Wyoming", "DC" => "Distric of Columbia"}
+      "WV" => "West Virginia", "WI" => "Wisconsin", "WY" => "Wyoming", "DC" => "District of Columbia"}
   end
 end
