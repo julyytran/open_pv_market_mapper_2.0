869dcafd497ffa945819c0c018e5f65ff7031356
July Tran <julyytran@gmail.com>
2016-04-27 20:25:21 -0600

    all layers actually rendering on and off correctly

--- app/assets/javascripts/render_map.js
+++ app/assets/javascripts/render_map.js
@@ -49,6 +49,34 @@ function renderMap(statesData){
 
    var timeLapse = L.mapbox.styleLayer('mapbox://styles/julyytran/cinji91jy001hadnjt6mazqnj')
 
+   var style =
+     'Map {' +
+     '-torque-time-attribute: "date";' +
+     '-torque-aggregation-function: "count(cartodb_id)";' +
+     '-torque-frame-count: 760;' +
+     '-torque-animation-duration: 17;' +
+     '-torque-resolution: 1' +
+     '}' +
+     '#layer {' +
+     '  marker-width: 2;' +
+     '  marker-fill-opacity: 1;' +
+     '  marker-fill: #0F3B82; ' +
+     '  comp-op: "lighten";' +
+     '  [value > 2] { marker-fill: #A0F4FF; }' +
+     '  [value > 7] { marker-fill: #FFFFFF; }' +
+     '  [frame-offset = 1] { marker-width: 10; marker-fill-opacity: 0.05;}' +
+     '  [frame-offset = 2] { marker-width: 20; marker-fill-opacity: 0.02;}' +
+     '}';
+
+     var torqueLayer = new L.TorqueLayer({
+       user: 'julyytran',
+       table: 'installs_data',
+       cartocss: style,
+       blendmode: 'lighter',
+       tiler_protocol: 'https',
+       tiler_port: 443
+     });
+
   var $select = $('<select></select>')
     .appendTo($('#variables'))
     .on('change', function() {
@@ -56,18 +84,24 @@ function renderMap(statesData){
         map.removeLayer(usLayer)
         renderTimeLapse();
       } else {
-        var b = document.querySelector("#variables");
-        var property = b.getAttribute( "data-name" );
+        debugger
+        map.removeLayer(timeLapse);
+        map.removeLayer(torqueLayer);
+        map.addLayer(usLayer)
+        setVariable($(this).val());
 
-        // debugger
-          if (property == "time-lapse") {
-            map.removeLayer(timeLapse);
-            map.addLayer(usLayer)
-            setVariable($(this).val());
-          } else {
-            map.addLayer(usLayer)
-            setVariable($(this).val());
-          }
+        // var b = document.querySelector("#variables");
+        // var property = b.getAttribute( "data-name" );
+          //
+          // if (typeof timeLapse !== 'undefined') {
+          //   debugger
+          //   map.removeLayer(timeLapse);
+          //   map.addLayer(usLayer)
+          //   setVariable($(this).val());
+          // } else {
+          //   map.addLayer(usLayer)
+          //   setVariable($(this).val());
+          // }
       }
     });
 
@@ -228,33 +262,33 @@ function renderTimeLapse() {
 
   // new L.Control.Zoom({ position: 'bottomleft' }).addTo(map);
 
-  var style =
-    'Map {' +
-    '-torque-time-attribute: "date";' +
-    '-torque-aggregation-function: "count(cartodb_id)";' +
-    '-torque-frame-count: 760;' +
-    '-torque-animation-duration: 17;' +
-    '-torque-resolution: 1' +
-    '}' +
-    '#layer {' +
-    '  marker-width: 2;' +
-    '  marker-fill-opacity: 1;' +
-    '  marker-fill: #0F3B82; ' +
-    '  comp-op: "lighten";' +
-    '  [value > 2] { marker-fill: #A0F4FF; }' +
-    '  [value > 7] { marker-fill: #FFFFFF; }' +
-    '  [frame-offset = 1] { marker-width: 10; marker-fill-opacity: 0.05;}' +
-    '  [frame-offset = 2] { marker-width: 20; marker-fill-opacity: 0.02;}' +
-    '}';
+  // var style =
+  //   'Map {' +
+  //   '-torque-time-attribute: "date";' +
+  //   '-torque-aggregation-function: "count(cartodb_id)";' +
+  //   '-torque-frame-count: 760;' +
+  //   '-torque-animation-duration: 17;' +
+  //   '-torque-resolution: 1' +
+  //   '}' +
+  //   '#layer {' +
+  //   '  marker-width: 2;' +
+  //   '  marker-fill-opacity: 1;' +
+  //   '  marker-fill: #0F3B82; ' +
+  //   '  comp-op: "lighten";' +
+  //   '  [value > 2] { marker-fill: #A0F4FF; }' +
+  //   '  [value > 7] { marker-fill: #FFFFFF; }' +
+  //   '  [frame-offset = 1] { marker-width: 10; marker-fill-opacity: 0.05;}' +
+  //   '  [frame-offset = 2] { marker-width: 20; marker-fill-opacity: 0.02;}' +
+  //   '}';
 
-  var torqueLayer = new L.TorqueLayer({
-    user: 'julyytran',
-    table: 'installs_data',
-    cartocss: style,
-    blendmode: 'lighter',
-    tiler_protocol: 'https',
-    tiler_port: 443
-  });
+  // var torqueLayer = new L.TorqueLayer({
+  //   user: 'julyytran',
+  //   table: 'installs_data',
+  //   cartocss: style,
+  //   blendmode: 'lighter',
+  //   tiler_protocol: 'https',
+  //   tiler_port: 443
+  // });
 
   torqueLayer.addTo(map);
   torqueLayer.play();
